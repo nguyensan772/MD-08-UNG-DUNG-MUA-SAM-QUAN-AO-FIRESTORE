e58ff456e01439a44ffaa4a2609a229c5f8ca14e
Sandapchai <nguyensan77@gmail.com>
2025-11-02 03:15:25 +0700

    man trang chu_ yeu thich

--- app/src/main/java/com/example/md_08_ungdungfivestore/fragments/TrangChuFragment.java
+++ app/src/main/java/com/example/md_08_ungdungfivestore/fragments/TrangChuFragment.java
@@ -1,4 +1,24 @@
 package com.example.md_08_ungdungfivestore.fragments;
 
-public class TrangChuFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.md_08_ungdungfivestore.R;
+
+public class TrangChuFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_man_trang_chu, container ,false);
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/md_08_ungdungfivestore/fragments/YeuThichFragment.java
+++ app/src/main/java/com/example/md_08_ungdungfivestore/fragments/YeuThichFragment.java
@@ -1,4 +1,21 @@
 package com.example.md_08_ungdungfivestore.fragments;
 
-public class YeuThichFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.md_08_ungdungfivestore.R;
+
+public class YeuThichFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_man_yeu_thich, container ,false);
+    }
 }
