50dfe99c7a583d8f39d17135b3c8fb35508f582e
DoTrungHieu2004 <hieu10productions@gmail.com>
2025-11-04 21:19:23 +0700

    Ánh xạ màn hình đặt hàng và fragment sản phẩm yêu thích

--- app/src/main/java/com/example/md_08_ungdungfivestore/ManDatHang.java
+++ app/src/main/java/com/example/md_08_ungdungfivestore/ManDatHang.java
@@ -1,14 +1,23 @@
 package com.example.md_08_ungdungfivestore;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ManDatHang extends AppCompatActivity {
+    private TextView diaChiTxt, hoTenKhachHangTxt, soDienThoaiTxt, emailKhachHangTxt, tongSoTienTxt, nutThanhToanTxt;
+    private ImageButton quayLaiBtn;
+    private RadioButton thanhToanRadioBtn, thanhToanTruocRadioBtn;
+    private RecyclerView danhSachMuaRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,26 @@ public class ManDatHang extends AppCompatActivity {
             return insets;
         });
 
+        anhXa();
 
+        quayLaiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void anhXa() {
+        diaChiTxt = findViewById(R.id.diaChiTxt);
+        hoTenKhachHangTxt = findViewById(R.id.hoTenKhachHangTxt);
+        soDienThoaiTxt = findViewById(R.id.soDienThoaiTxt);
+        emailKhachHangTxt = findViewById(R.id.emailKhachHangTxt);
+        tongSoTienTxt = findViewById(R.id.tongSoTienTxt);
+        nutThanhToanTxt = findViewById(R.id.nutThanhToanTxt);
+        quayLaiBtn = findViewById(R.id.quayLaiBtn);
+        thanhToanRadioBtn = findViewById(R.id.thanhToanRadioBtn);
+        thanhToanTruocRadioBtn = findViewById(R.id.thanhToanTruocRadioBtn);
+        danhSachMuaRecyclerView = findViewById(R.id.danhSachMuaRecyclerView);
     }
 }
