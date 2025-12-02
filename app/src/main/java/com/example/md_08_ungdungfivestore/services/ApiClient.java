77ba5b01a39f534e724b6cd5d9b7f5f4b66f1624
r4nd1 <150415580+ducnvph36848@users.noreply.github.com>
2025-12-02 14:43:21 +0700

    mercode

--- app/src/main/java/com/example/md_08_ungdungfivestore/services/ApiClient.java
+++ app/src/main/java/com/example/md_08_ungdungfivestore/services/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
-    private static final String BASE_URL = "http://localhost:5002/";
+    private static final String BASE_URL = "http://10.0.2.2:5001/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
